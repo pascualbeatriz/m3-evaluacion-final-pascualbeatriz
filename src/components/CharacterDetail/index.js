70c9d9c51eab395f8e5ddacebd4bd13b6191a5aa
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-09-23 19:47:14 +0200

    Resolve warnings in CharacterDetail component

--- src/components/CharacterDetail/index.js
+++ src/components/CharacterDetail/index.js
@@ -7,23 +7,30 @@ const CharacterDetail = props => {
   const getId = parseInt(routerProps.match.params.id);
 
   const characterDetail = characters.filter(detail => detail.id === getId);
-  console.log(characterDetail);
-  const {name,status,species,origin,episode,image} = characterDetail[0];
-  // tengo que pasarle name,status,origen,episode,imagen
-  return(
-    <Fragment>
-    <Link to="/" className="character__List-Home-link"/>
-      <h3 className="detail-name">{name}</h3>
-      <p className="detail-status">{status}</p>
-      <p className="detail-species">{species}</p>
-      <p className="detail-origen">{origin.name}</p>
-      <p className="detail-episode">{episode.length}</p>
-      <div className="container__detail-img">
-        <img src={image} alt={`Imagen de ${name}`} className="detail_img"/> 
-      </div>
-    
-    </Fragment>
-  );
+
+  if(characterDetail[0  ]){
+    const {name,status,species,origin,episode,image} = characterDetail[0];
+      return(
+        <Fragment>
+          <Link to="/" className="character__List-Home-link"> Volver </Link>
+            <h3 className="detail-name">{`Name: ${name}`}</h3>
+            <p className="detail-status">{`Status: ${status}`}</p>
+            <p className="detail-species">{`Species: ${species}`}</p>
+            <p className="detail-origen">{`Origin: ${origin.name}`}</p>
+            <p className="detail-episode">{`Episodes: ${episode.length}`}</p>
+            <div className="container__detail-img">
+              <img src={image} alt={`Imagen de ${name}`} className="detail_img"/> 
+            </div>
+        </Fragment>
+      );
+  }else{
+    return(
+      <Fragment>
+        <p className="warning__messagge">Ese personaje no existe, intentalo de nuevo</p>
+        <Link to="/" className="character__List-Home-link"> Volver </Link>
+      </Fragment>
+    );
+  }
 };
 
 CharacterDetail.propTypes = {
