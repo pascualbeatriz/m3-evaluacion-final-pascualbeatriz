cbca51504d55c1abde49520fe7e18acfa36c5917
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-09-23 10:51:01 +0200

    Show images in the browser

--- src/App.js
+++ src/App.js
@@ -19,16 +19,31 @@ getUrl(){
   getUrlData()
   .then(data => {
     this.setState({
-      characters:data
+      characters:data.results
     });
-  })
-
+  });
 }
 
   render() {
+    
     return (
-      <div className="App">
-       
+      <div className="app">
+        <h1 className="app__tittle">titulo, aqui va el logo</h1>
+        <ul className="characters__list">
+        {this.state.characters.map(characterList => {
+          return(
+            <li className="character__List" key={characterList.id}>
+              <div className="character__List-container">
+                <h2 className="character__List-name">{characterList.name}</h2>
+                <div className="character__img-container">
+                  <img src={characterList.image} alt={`imagen de ${characterList.name}`} className="character__img"/>
+                </div>
+                <p className="character__species">{characterList.species}</p>
+              </div>
+            </li>
+          );
+        })}
+        </ul>       
       </div>
     );
   }
