d7ee5d444f1cef982a157094f7acdf44e126b531
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-09-24 13:18:00 +0200

    Add new-docs

---
+++ docs/build/precache-manifest.9d6ceef4562a69de3e58a62acfb27ffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bcaffb120dc20e58ca0234004a9b47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74c2926e783e550c052d",
+    "url": "./static/css/main.bbecde6e.chunk.css"
+  },
+  {
+    "revision": "ce831bd7eb0d4176e8f7",
+    "url": "./static/js/2.93a1ba85.chunk.js"
+  },
+  {
+    "revision": "74c2926e783e550c052d",
+    "url": "./static/js/main.6bcd3dd2.chunk.js"
+  },
+  {
+    "revision": "5f0b5866259ee39f4445",
+    "url": "./static/js/runtime-main.f3018116.js"
+  }
+]);
